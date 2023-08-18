36f55072770886acdd0a5291e5ad78ac487ebf35
renata-aline <renatalinec@gmail.com>
2023-08-18 12:33:53 -0300

    salvando a foto

--- js/camera.js
+++ js/camera.js
@@ -3,13 +3,14 @@ const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
-const mensagem = document.querySelector("[data-mensagem]"); 
+const mensagem = document.querySelector("[data-mensagem]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]");
 
 let imagemURL = "";
 
-botaoIniciarCamera.addEventListener("click",async function (){
+botaoIniciarCamera.addEventListener("click", async function () {
     const iniciarVideo = await navigator.mediaDevices
-    .getUserMedia({video: true,audio: false})
+        .getUserMedia({ video: true, audio: false })
 
     botaoIniciarCamera.style.display = "none";
     campoCamera.style.display = "block";
@@ -17,11 +18,22 @@ botaoIniciarCamera.addEventListener("click",async function (){
     video.srcObject = iniciarVideo;
 })
 
-botaoTirarFoto.addEventListener("click", function(){
-   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   
-   imagemURL = canvas.toDataURL("imagem/jpeg");
+botaoTirarFoto.addEventListener("click", function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 
-   campoCamera.style.display = "none";
-   mensagem.style.display = "block";
+    imagemURL = canvas.toDataURL("imagem/jpeg");
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block";
+})
+
+botaoEnviarFoto.addEventListener("click", () => {
+    const receberDadosExistentes = localStorage.getItem("cadastro");
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+
+    window.location.href = "./abrir-conta-form-3.html";
 })
