efb48d715b1a9056a245f05a414ade011efde80b
renata-aline <renatalinec@gmail.com>
2023-08-18 12:08:23 -0300

    capiturando imagem

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,11 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]"); 
+
+let imagemURL = "";
 
 botaoIniciarCamera.addEventListener("click",async function (){
     const iniciarVideo = await navigator.mediaDevices
@@ -10,4 +15,13 @@ botaoIniciarCamera.addEventListener("click",async function (){
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
+})
+
+botaoTirarFoto.addEventListener("click", function(){
+   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   
+   imagemURL = canvas.toDataURL("imagem/jpeg");
+
+   campoCamera.style.display = "none";
+   mensagem.style.display = "block";
 })
